b1c9da61587f3c598fc2ed8aacb395998333b026
whj <whj@DESKTOP-5I9AEMK>
2021-03-03 16:56:33 +0900

    hp_v1

---
+++ crawling/csv_to_json.py
@@ -0,0 +1,76 @@
+import csv
+import json
+import pandas as pd
+'''
+[
+    {
+        "year":"2020",
+        "publications":[
+            {
+                "title":"bl;dufl",
+                "writer":"writer x",
+                "academy":"acm 2020",
+                "pdf":"www.naver.com",
+                "section":"1"
+            },
+            {
+                "title":"bl;dufldfs",
+                "writer":"writer x, writer xy",
+                "academy":"kdd 2020",
+                "poster":"www.naver.com",
+                "ppt":"www.daum.net",
+                "section":"2"
+               
+            }
+        ]
+    },
+    {
+        "year":"2019",
+        "publications":[
+            {
+                "title":"bl;dufl",
+                "writer":"writer x",
+                "academy":"acm 2019",
+                "section":"2"
+            },
+            {
+                "title":"bl;dufldfs",
+                "writer":"writer x, writer xy",
+                "academy":"kdd 2019",
+                "section":"1"
+            }
+        ]
+    }
+]
+'''
+
+data = pd.read_csv('crawled_data.csv')
+
+
+years = data['year'].unique().tolist()
+years.sort(key= lambda x: -int(x))
+
+arr = {}
+for year in years:
+    arr[year] = {"year": str(year), 'publications':[]}
+
+for i, row in data.iterrows():
+    if row.ws != row.ws:
+        row.ws = ""
+
+    arr[row['year']]['publications'].append(
+        {
+            "title": row.title,
+            "writer": row.writer,
+            "academy": row.ws,
+            "section": "1"
+        }
+    )
+
+final = list(arr.values())
+#print(final)
+
+json_val = json.loads(json.dumps(final, ))
+
+with open('real_publications.json', 'w') as f:
+    json.dump(json_val,f,indent=4,)
